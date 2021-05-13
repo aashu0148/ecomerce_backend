09755d03b8513c23b9645449b17fa499c884221e
aashu0148 <buildforfb@gmail.com>
2021-05-13 22:27:46 +0530

    filetr-search Api added

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const Product = require("../models/product");
 
-// brand | price | for(men,women,children) | size | type(casual,formal) | season(summer,winter,both)
+// "filters":{
+// "season":["summer"],
+// "for":["men","women"],
+//   "price":{
+//       "lte":"500",
+//       "gte":"1500"
+//   }
 
 router.post("/add", (req, res) => {
   const { title, price, image, sizes, size, desc, images, filters, tags } =
@@ -30,6 +36,8 @@ router.post("/add", (req, res) => {
       season: filters.season,
       for: filters.for,
       brand: filters.brand,
+      price: price,
+      sizes: sizes,
     },
     tags,
   });
@@ -49,6 +57,63 @@ router.post("/add", (req, res) => {
         error: err,
       });
     });
+});
+
+router.post("/filter-search", async (req, res) => {
+  const filters = req.body.filters;
+
+  const myArray = [];
+
+  if (filters.type) {
+    myArray.push({
+      "filters.type": { $in: filters.type },
+    });
+  }
+  if (filters.brand) {
+    myArray.push({
+      "filters.brand": { $in: filters.brand },
+    });
+  }
+  if (filters.size) {
+    myArray.push({
+      "filters.sizes": { $in: filters.size },
+    });
+  }
+  if (filters.for) {
+    myArray.push({
+      "filters.for": { $in: filters.for },
+    });
+  }
+  if (filters.season) {
+    myArray.push({
+      "filters.season": { $in: filters.season },
+    });
+  }
+  if (filters.price) {
+    myArray.push({
+      "filters.price": { $lte: filters.price.lte, $gte: filters.price.gte },
+    });
+  }
+
+  const result = await Product.find(
+    {
+      $and: myArray,
+    },
+    "-filters -tags"
+  );
+  if (result.length == 0) {
+    res.status(404).json({
+      status: false,
+      message: "Nothing Found !",
+    });
+    return;
+  }
+  res.status(200).json({
+    status: true,
+    data: result,
+    message: "Got data successfully",
+  });
+  return;
 });
 
 router.delete("/:id", (req, res) => {
@@ -130,6 +195,27 @@ router.get("/", async (req, res) => {
   res.status(200).json({
     status: true,
     data: result,
+    // filters:[{
+    //   name:"Price",
+    //   values:[
+    //     {
+    //       name: "Under 500",
+    //       value: "52",
+    //     },
+    //     {
+    //       name: "500 - 1000",
+    //       value: "18",
+    //     },
+    //     {
+    //       name: "1000-3000",
+    //       value: "17",
+    //     },
+    //     {
+    //       name: "3000+",
+    //       value: "16",
+    //     },
+    //   ],
+    // }],
     message: "Got data successfully",
   });
 });
