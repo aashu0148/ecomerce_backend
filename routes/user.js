16e05c6517c56d7e21cf0d4700dbbe241fc5431d
aashu0148 <buildforfb@gmail.com>
2021-05-14 12:33:49 +0530

    signin signup API improved

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const secretKey = require("../secret");
+
+router.post("/token-signin", async (req, res) => {
+  const token = req.body.token;
+  let email, password;
+  jwt.verify(token, secretKey, (err, data) => {
+    if (err) {
+      res.status(422).json({
+        status: false,
+        message: "Token Invalid",
+      });
+      return;
+    }
+    email = data.email;
+    password = data.password;
+  });
+
+  const result = await User.findOne(
+    { email: email, password: password },
+    "-password"
+  );
+
+  if (!result) {
+    res.status(404).json({
+      status: false,
+      message: "User not Found",
+    });
+    return;
+  }
+
+  res.status(200).json({
+    status: true,
+    data: result,
+    message: "User Found",
+  });
+});
 
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
@@ -24,7 +61,7 @@ router.post("/signin", async (req, res) => {
   });
 });
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   const { name, email, mobile, password } = req.body;
   if (!name) {
     res.status(422).json({
@@ -58,11 +95,20 @@ router.post("/signup", (req, res) => {
     return;
   }
 
+  const result = await User.findOne({ email: email }, "-password");
+  if (result) {
+    res.status(422).json({
+      status: false,
+      message: "Email already in use. Please Signin",
+    });
+    return;
+  }
   const newUser = new User({
     name,
     email,
     mobile,
     password,
+    cart: [],
   });
 
   newUser
